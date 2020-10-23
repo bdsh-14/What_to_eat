f210a20c9a99fa9bface3418a4d2b1bd8b7d321d
Bidisha <bbiswas@unomaha.edu>
2020-10-22 20:56:29 -0500

    Initialize the project on Xcode

---
+++ What_to_eat/What_to_eat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  What_to_eat
+//
+//  Created by Bidisha Biswas on 10/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
