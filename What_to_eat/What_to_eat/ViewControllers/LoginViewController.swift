226af44bf6f5a4967ffdfa7a8264659a1359f0c9
Bidisha <bbiswas@unomaha.edu>
2020-10-22 21:54:47 -0500

    create login vc

--- What_to_eat/What_to_eat/ViewControllers/LoginViewController.swift
+++ What_to_eat/What_to_eat/ViewControllers/LoginViewController.swift
@@ -9,21 +9,13 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "login_bg_img")!)
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
